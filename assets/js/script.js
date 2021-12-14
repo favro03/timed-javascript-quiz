65386d2d3cfa39faae8d81905c82e8676d435c30
tfavro <wetr9902@gmail.com>
2021-12-13 22:28:15 -0600

    added high score page, and main html page, also input header

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var body = document.body;
+var h2El = document.createElement("h2");
+var pEl = document.createElement("p");
+var lineBreak = '\n';
+
+
+
+h2El.textContent = "Coding Quiz Challenge";
+body.appendChild(h2El);
+
+pEl.textContent = 'Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
+body.appendChild(pEl);
+
+
+
+
+//Need an ordered list for the answers
+
+
+
+//timed quiz on JavaScript fundamentals that stores high scores
+
+//WHEN I click the start button
+//THEN a timer starts at 75 and I am presented with a question
+
+
+//WHEN I answer a question
+//THEN I am presented with another question
+
+
+//WHEN I answer a question incorrectly
+//THEN 10 seconds is subtracted from the clock
+
+
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+
+
+//WHEN the game is over
+//THEN I can save my initials and score
