0e5aabf213844d5988a691bbe3dd4fa988cbc8a0
tfavro <wetr9902@gmail.com>
2021-12-14 21:33:34 -0600

    click button timer starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
 var body = document.body;
+var div = document.div;
 var h2El = document.createElement("h2");
 var pEl = document.createElement("p");
-var lineBreak = '\n';
+var p2El = document.createElement("p");
+var p3El = document.createElement("p");
+var startQuiz = document.querySelector('#start-quiz');
+var countEl = document.querySelector('#time');
+var question1El=document.createElement("h2");
 
 
 
 h2El.textContent = "Coding Quiz Challenge";
 body.appendChild(h2El);
 
-pEl.textContent = 'Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
+pEl.textContent = 'Try to answer the following code related questions within the time limit.';
+p2El.textContent = 'Keep in mind that incorrect answers will penalize your score';
+p3El.textContent = 'time by ten seconds!';
 body.appendChild(pEl);
+body.appendChild(p2El);
+body.appendChild(p3El);
+
+function setCounterText(){
+    countEl.textContent = count;
+};
+
+startQuiz.addEventListener('click', function(){
+    count =70;
+    setCounterText();
+    //this is how data was replaced in taskinator, need to figure out how to replace above text with new text document.querySelector("input[name='task-name']").value = "";
+    question1El.textContent = 'Commonly used data types DO not Include:';
+    body.appendChild(question1El);
+
+})
+
+
 
 
 
