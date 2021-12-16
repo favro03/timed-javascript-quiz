de9f9f5954fb006e18064e525bbee2cba64a5b2c
tfavro <wetr9902@gmail.com>
2021-12-15 20:01:23 -0600

    click on quiz button and get first question, tiimer starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,36 @@
 var body = document.body;
-var div = document.div;
-var h2El = document.createElement("h2");
-var pEl = document.createElement("p");
-var p2El = document.createElement("p");
-var p3El = document.createElement("p");
+var infoEl = document.querySelector("#intro");
+var question1El = document.querySelector("#question1");
+var question2El = document.querySelector("#question2");
 var startQuiz = document.querySelector('#start-quiz');
 var countEl = document.querySelector('#time');
-var question1El=document.createElement("h2");
 
 
 
-h2El.textContent = "Coding Quiz Challenge";
-body.appendChild(h2El);
-
-pEl.textContent = 'Try to answer the following code related questions within the time limit.';
-p2El.textContent = 'Keep in mind that incorrect answers will penalize your score';
-p3El.textContent = 'time by ten seconds!';
-body.appendChild(pEl);
-body.appendChild(p2El);
-body.appendChild(p3El);
-
-function setCounterText(){
+var setCounterText =function(){
     countEl.textContent = count;
 };
 
 startQuiz.addEventListener('click', function(){
     count =70;
     setCounterText();
+    infoEl.style.display = 'none';
+    question1El.style.display = 'inline';
     //this is how data was replaced in taskinator, need to figure out how to replace above text with new text document.querySelector("input[name='task-name']").value = "";
-    question1El.textContent = 'Commonly used data types DO not Include:';
-    body.appendChild(question1El);
-
-})
-
-
-
+   // h2El.textContent = 'Commonly used data types DO not Include:';
+    //pEl.remove();
+   // startQuiz.remove();
+   
+    //btnAnswer1.innerHTML = "1. string"; 
+    //btnAnswer2.innerHTML = "2. booleans"; 
+    //btnAnswer3.innerHTML = "3. alerts"; 
+    //btnAnswer4.innerHTML = "4. numbers";   
+    //appendButtons();
+    //formatButtons();
+    
+    //do an if else and event listners to the buttons to if they click the right one go to next q, else wrong answer and =tinen taken away
+   
+});
 
 
 
