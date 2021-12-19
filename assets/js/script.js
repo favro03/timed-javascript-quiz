14f93e6f81822a698846571587836fc8a82d22e5
tfavro <wetr9902@gmail.com>
2021-12-19 14:47:28 -0600

    styling complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,17 +18,19 @@ var startQuizEl = document.querySelector('#start-quiz');
 var timerEl = document.getElementById('time');
 var correctAnswer = "";
 var quizEl = document.getElementById('right-wrong');
-
 var timeLeft = 75;
+
 
 
 quizEl.style.display = 'none';
 startQuizEl.addEventListener('click', function(){
+    
     infoEl.style.display = 'none';
     timer();
-    
     question1();  
 });
+
+    
 
 var timer = function(){
     
@@ -228,7 +230,7 @@ var question5 = function() {
 
 //need to fix the timer removal here I want the timer to show 0, and I think maybe use the html for the score stuff and hide it in css and just show it in thsi function
 var end = function(){
-    questionEl.style.display ='All Done!';
+   
     answer1El.style.display ='none';
     answer2El.style.display ='none';
     answer3El.style.display ='none';
@@ -237,7 +239,7 @@ var end = function(){
     questionEl.textContent = 'All done!';
     finalScoreEl.textContent = 'Your final score is ' + timeLeft;
     //initialsEl.textContent = 'Enter initials: ';
-    formEl.style.display = 'flex';
+    formEl.style.display = 'flex    ';
     answerResponse();
     return true;
     
@@ -283,7 +285,7 @@ var answerResponse = function(){
     
 };
 
-
+//See if this is even working
 var initialFormHandler = function (event) {
     event.preventDefault();
     var initialNameInput = document.querySelector("input[name='user-initials']").value;
@@ -307,247 +309,7 @@ var initialFormHandler = function (event) {
       };
   };
 
+
+
+
   
-//NEED to get the end to work and the timer to work, and need to store score
-
-/*
-
-var doneEl = document.querySelector("#done");
-var finalScoreEl = document.getElementById('final-score');
-
-var correctAnswer=false;
-
-var countdown = function(){
-    var count =40;
-    startQuiz.addEventListener('click', function(){
-        var counter = setInterval(function(){
-            if (count > 0){
-                timerEl.textContent = 'Time: ' + count;
-                count --;
-            }
-            else if (count <= 0){
-                end();
-            }
-        }, 1000);
-
-        infoEl.style.display = 'none';
-        question1El.style.display='block';
-    });
-//question1
-};
-
-var q1 = function(){
-    btnQ1Answer1.addEventListener('click', function(){
-        question1();
-    });
-    btnQ1Answer2.addEventListener('click', function(){ 
-        question1();
-    });
-    btnQ1Answer3.addEventListener('click', function(){
-        correctAnswer = true;
-        question1();
-    });
-    btnQ1Answer4.addEventListener('click', function(){
-        question1();
-    });
-};
-
-var question1 = function(){
-    if (correctAnswer === true){
-        question1El.style.display ='none';
-        question2El.style.display ='block';
-        correct1El.style.display = 'block';
-    }
-    else {
-        question1El.style.display ='none';
-        question2El.style.display ='block';
-        wrong1El.style.display = 'block';
-        count =- 10; 
-    }
-correctAnswer=false;
-};
-//Question2
-var q2 = function(){
-    btnQ2Answer1.addEventListener('click', function(){
-        question2();
-    });
-    btnQ2Answer2.addEventListener('click', function(){ 
-        question2();
-    });
-    btnQ2Answer3.addEventListener('click', function(){
-        correctAnswer = true;
-        question2();
-    });
-    btnQ2Answer4.addEventListener('click', function(){
-        question2();
-    });
-};
-var question2 = function(){
-    if (correctAnswer === true){
-        question2El.style.display ='none';
-        question3El.style.display = 'block';
-        correct1El.style.display = 'none';
-        wrong1El.style.display = 'none';
-        correct2El.style.display = 'block';
-    }
-    else {
-        question2El.style.display ='none';
-        question3El.style.display = 'block';
-        wrong1El.style.display = 'none';
-        correct1El.style.display = 'none';
-        wrong2El.style.display = 'block';  
-        count =- 10; 
-    }
-correctAnswer = false;
-};
-
-//question 3
-var q3 = function(){
-    btnQ3Answer1.addEventListener('click', function(){
-        question3();
-    });
-    btnQ3Answer2.addEventListener('click', function(){ 
-        question3();
-    });
-    btnQ3Answer3.addEventListener('click', function(){
-        
-        question3();
-    });
-    btnQ3Answer4.addEventListener('click', function(){
-        correctAnswer = true;
-        question3();
-    });
-};
-
-var question3 = function(){
-    if (correctAnswer === true){
-        question3El.style.display ='none';
-        question4El.style.display = 'block';
-        correct2El.style.display = 'none';
-        wrong2El.style.display = 'none';
-        correct3El.style.display = 'block';
-    }
-    else {
-        question3El.style.display ='none';
-        question4El.style.display = 'block';
-        wrong2El.style.display = 'none';
-        correct2El.style.display = 'none';
-        wrong3El.style.display = 'block';
-        count =- 10;
-    }
-correctAnswer = false;
-};
-
-//question 4
-var q4 = function(){
-    btnQ4Answer1.addEventListener('click', function(){
-        question4();
-    });
-    btnQ4Answer2.addEventListener('click', function(){ 
-        question4();
-    });
-    btnQ4Answer3.addEventListener('click', function(){
-        question4();
-    });
-    btnQ4Answer4.addEventListener('click', function(){
-        correctAnswer = true;
-        question4();
-    });
-};
-var question4 = function(){
-    if (correctAnswer === true){
-        question4El.style.display ='none';
-        question5El.style.display = 'block';
-        correct3El.style.display = 'none';
-        wrong3El.style.display = 'none';
-        correct4El.style.display = 'block';
-    }
-    else {
-        question4El.style.display ='none';
-        question5El.style.display = 'block';
-        wrong3El.style.display = 'none';
-        correct3El.style.display = 'none';
-        wrong4El.style.display = 'block';
-        count =- 10;
-    }
-correctAnswer = false;
-};
-
-//question 5
-var q5 = function(){
-    btnQ5Answer1.addEventListener('click', function(){
-        question5();
-    });
-    btnQ5Answer2.addEventListener('click', function(){ 
-        question5();
-    });
-    btnQ5Answer3.addEventListener('click', function(){
-        question5();
-    });
-    btnQ5Answer4.addEventListener('click', function(){
-        correctAnswer = true;
-        question5();
-    });
-}
-var question5 = function(){
-    if (correctAnswer === true){
-        question5El.style.display ='none';
-        doneEl.style.display = 'block';
-        correct4El.style.display = 'none';
-        wrong4El.style.display = 'none';
-        correct5El.style.display = 'block';
-    }
-    else {
-        question5El.style.display ='none';
-        doneEl.style.display = 'block';
-        wrong4El.style.display = 'none';
-        correct4El.style.display = 'none';
-        wrong5El.style.display = 'block';
-        count =- 10;
-        //add local storage or function for local storage
-        //add function for data input if its not initials
-    }
-correctAnswer = false;
-};
-
-var end = function(){
-    doneEl.style.display = 'block';
-    finalScoreEl.textContent = 'Your final score is' + count;
-    //Need to get the p element + count
-    //add local storage or function for local storage
-    //add function for data input if its not initials
-};
-
-
-
-
-
-//timed quize starting with 75 seconds, if you get it wrong it takes off an additional 10 seconds
-
-
-
-//timed quiz on JavaScript fundamentals that stores high scores
-
-
-
-
-
-
-
-//WHEN I answer a question incorrectly
-//THEN 10 seconds is subtracted from the clock
-
-
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-
-
-//WHEN the game is over
-//THEN I can save my initials and score
-countdown();
-q1();
-q2();
-q3();
-q4();
-q5();
-*/
